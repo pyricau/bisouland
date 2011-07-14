0a31cae0911df47bde5bad1af75c1ef62ad33410
gnugat <loic.chardonnet@gmail.com>
2011-07-14 13:36:00 +0200

    Creation de la version 2 avec le micro framework Atomik

---
+++ web2/app/config.php
@@ -0,0 +1,26 @@
+<?php
+
+Atomik::set(array(
+  'app' => array(
+      'layout'          => '_layout',
+      'default_action'  => 'index',
+      'views'           => array(
+          'file_extension'  => '.phtml',
+      ),
+  ),
+  'atomik' => array(
+      'start_session'   => true,
+      'class_autoload'  => true,
+      'trigger'         => 'action',
+      'catch_errors'    => true,
+      'display_errors'  => true,
+      'debug'           => false,
+  ),
+  'styles' => array(
+      0                 => 'assets/css/main.css',
+  ),
+  'plugins' => array(
+  ),
+  'scripts' => array(
+  ),
+));

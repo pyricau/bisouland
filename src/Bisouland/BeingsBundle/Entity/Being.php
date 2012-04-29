448bff056ab38249ca60633cfa2f50be6d671ae1
Loïc Chardonnet <loic.chardonnet@gmail.com>
2012-04-29 11:55:19 +0100

    [characters] generation de l'entite

---
+++ src/Bisouland/BeingsBundle/Entity/Being.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Bisouland\BeingsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Bisouland\BeingsBundle\Entity\Being
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Being
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $name
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Being
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}

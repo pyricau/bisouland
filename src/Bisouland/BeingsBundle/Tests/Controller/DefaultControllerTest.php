144652685c8c716aa343257d4e3f4b51af24d3c5
Loïc Chardonnet <loic.chardonnet@gmail.com>
2012-04-30 14:18:24 +0100

    [characters] test unitaire pour les naissances

--- src/Bisouland/BeingsBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Bisouland/BeingsBundle/Tests/Controller/DefaultControllerTest.php
@@ -3,7 +3,7 @@
 namespace Bisouland\BeingsBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Bisouland\BeingsBundle\DataFixtures\ORM\LoadBeingData;
+use Bisouland\BeingsBundle\DataFixtures\ORM\LoadBeingData; 
 
 class DefaultControllerTest extends WebTestCase
 {
@@ -25,6 +25,29 @@ class DefaultControllerTest extends WebTestCase
         $beings = LoadBeingData::getFixtures();
         foreach ($beings as $being) {
             $this->assertTrue(0 < $crawler->filter('td:contains("'.$being['name'].'")')->count());
+        }
+    }
+    
+    public function testBirth()
+    {
+        $client = static::createClient();
+        $em = $client->getContainer()->get('doctrine.orm.entity_manager');
+        
+        $routes = array(
+            '/beings/',
+        );
+        foreach ($routes as $route) {
+            $beings = $em->getRepository('BisoulandBeingsBundle:Being')
+                    ->findAll();
+            $numberBefore = count($beings);
+
+            $client->request('GET', $route);
+            
+            $beings = $em->getRepository('BisoulandBeingsBundle:Being')
+                    ->findAll();
+            $numberAfter = count($beings);
+
+            $this->assertTrue(1 === $numberAfter - $numberBefore);
         }
     }
 }

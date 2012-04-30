279d31ef281c4d66669e860b08fef485e1351809
Loïc Chardonnet <loic.chardonnet@gmail.com>
2012-04-30 11:01:30 +0100

    [characters] Creation automatique de personnages

--- src/Bisouland/BeingsBundle/Controller/DefaultController.php
+++ src/Bisouland/BeingsBundle/Controller/DefaultController.php
@@ -6,8 +6,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+use Bisouland\BeingsBundle\Filters\Before;
+use Bisouland\BeingsBundle\Entity\Being;
+
+/**
+ * @Before("beforeFilter")
+ */
 class DefaultController extends Controller
 {
+    public function beforeFilter()
+    {
+        $randomName = mt_rand();
+
+        $newBeing = new Being();
+        $newBeing->setName("$randomName");
+
+        $em = $this->getDoctrine()->getEntityManager();
+        $em->persist($newBeing);
+        $em->flush();
+    }
+
     /**
      * @Route("/")
      * @Template()

c169abc98837c8e1fc692356418e92c80fe0751c
gnugat <loic.chardonnet@gmail.com>
2012-05-10 14:59:08 +0100

    [GlobalBirth] Refactoring des tests d'auto generation

--- src/Bisouland/BeingsBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Bisouland/BeingsBundle/Tests/Controller/DefaultControllerTest.php
@@ -5,7 +5,6 @@ namespace Bisouland\BeingsBundle\Tests\Controller;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Bisouland\BeingsBundle\DataFixtures\ORM\LoadBeingData; 
 use Bisouland\BeingsBundle\Controller\DefaultController;
-use Bisouland\BeingsBundle\Controller\StatisticsController;
 
 class DefaultControllerTest extends WebTestCase
 {
@@ -28,38 +27,5 @@ class DefaultControllerTest extends WebTestCase
         foreach ($beings as $being) {
             $this->assertTrue(0 < $crawler->filter('td:contains("'.$being['name'].'")')->count());
         }
-    }
-    
-    public function testBeingGeneration()
-    {
-        $client = static::createClient();
-        $em = $client->getContainer()->get('doctrine.orm.entity_manager');
-        
-        $routes = array(
-            '/beings/',
-        );
-        foreach ($routes as $route) {
-            $numberBefore = $em->getRepository('BisoulandBeingsBundle:Being')->countAlivePopulation();
-
-            $client->request('GET', $route);
-            
-            $numberAfter = $em->getRepository('BisoulandBeingsBundle:Being')->countAlivePopulation();
-
-            $this->assertTrue(1 === $numberAfter - $numberBefore);
-        }
-    }
-    
-    public function testNumberMaxOfBirthPerDay()
-    {
-        $client = static::createClient();
-        $em = $client->getContainer()->get('doctrine.orm.entity_manager');
-
-        for ($numberOfBirth = 0; $numberOfBirth < StatisticsController::$numberMaxOfBirthPerDay; $numberOfBirth++) {
-            $client->request('GET', '/beings/');
-        }
-        
-        $client->request('GET', '/beings/');
-
-        $this->assertTrue(StatisticsController::$numberMaxOfBirthPerDay == $em->getRepository('BisoulandBeingsBundle:Being')->countBirthsToday());
     }
 }

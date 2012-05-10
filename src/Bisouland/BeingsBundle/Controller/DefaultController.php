ef896f8134045564e1ec3f12b11289f8aae009b7
gnugat <loic.chardonnet@gmail.com>
2012-05-10 13:40:06 +0100

    [GlobalBirth] Les naissances sont globales

--- src/Bisouland/BeingsBundle/Controller/DefaultController.php
+++ src/Bisouland/BeingsBundle/Controller/DefaultController.php
@@ -6,37 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-use Bisouland\BeingsBundle\Filters\Before;
-use Bisouland\BeingsBundle\Entity\Being;
-
-/**
- * @Before("beforeFilter")
- */
 class DefaultController extends Controller
 {
-    static public $numberMaxOfBirthPerDay = 42;
-
-    public function beforeFilter()
-    {
-        $numberOfBirthsToday = $this->getDoctrine()
-                ->getRepository('BisoulandBeingsBundle:Being')
-                ->countBirthsToday();
-        
-        if (self::$numberMaxOfBirthPerDay > $numberOfBirthsToday) {
-            $nameLength = mt_rand(4, 9);
-
-            $nameGenerator = $this->container->get('pronounceable_word_generator');
-            $randomName = ucfirst($nameGenerator->generateWordOfGivenLength($nameLength));
-
-            $newBeing = new Being();
-            $newBeing->setName($randomName);
-
-            $em = $this->getDoctrine()->getEntityManager();
-            $em->persist($newBeing);
-            $em->flush();
-        }
-    }
-
     /**
      * @Route("/", name="beings")
      * @Template()
--- src/Bisouland/BeingsBundle/Controller/StatisticsController.php
+++ src/Bisouland/BeingsBundle/Controller/StatisticsController.php
@@ -5,10 +5,42 @@ namespace Bisouland\BeingsBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+use Bisouland\BeingsBundle\Filters\Before;
 use Bisouland\BeingsBundle\Entity\Being;
 
+/**
+ * @Before("beforeFilter")
+ */
 class StatisticsController extends Controller
 {
+    static public $numberMaxOfBirthPerDay = 42;
+
+    public function beforeFilter()
+    {
+        $this->birthGenerator();
+    }
+
+    protected function birthGenerator()
+    {
+        $numberOfBirthsToday = $this->getDoctrine()
+                ->getRepository('BisoulandBeingsBundle:Being')
+                ->countBirthsToday();
+        
+        if (self::$numberMaxOfBirthPerDay > $numberOfBirthsToday) {
+            $nameLength = mt_rand(4, 9);
+
+            $nameGenerator = $this->container->get('pronounceable_word_generator');
+            $randomName = ucfirst($nameGenerator->generateWordOfGivenLength($nameLength));
+
+            $newBeing = new Being();
+            $newBeing->setName($randomName);
+
+            $em = $this->getDoctrine()->getEntityManager();
+            $em->persist($newBeing);
+            $em->flush();
+        }
+    }
+
     /**
      * @Template()
      */

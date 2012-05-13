a2de1b3c33f9332e6e42af67e28b6c159c68ae10
gnugat <loic.chardonnet@gmail.com>
2012-05-13 14:18:44 +0100

    [LovePoints] Mise a jour automatique des PA

--- src/Bisouland/BeingsBundle/Controller/DefaultController.php
+++ src/Bisouland/BeingsBundle/Controller/DefaultController.php
@@ -6,8 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+use Bisouland\BeingsBundle\Filters\Before;
+
+/**
+ * @Before("beforeFilter") 
+ */
 class DefaultController extends Controller
 {
+    public function beforeFilter()
+    {
+        $this->updateLovePoints();
+    }
+    
+    private function updateLovePoints()
+    {
+        $entityManager = $this->getDoctrine()->getEntityManager();
+        $beings = $entityManager->getRepository('BisoulandBeingsBundle:Being')->findAll();
+        foreach ($beings as $being) {
+            $being->setLovePoints($being->getLovePoints());
+        }
+        $entityManager->flush();
+    }
+
     /**
      * @Route("/", name="beings")
      * @Template()

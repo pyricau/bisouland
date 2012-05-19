ba65067e0b255148ea334f7af905872ac13bae66
gnugat <loic.chardonnet@gmail.com>
2012-05-19 10:58:01 +0100

    [FlashMessages] Utilisation du controlleur Update pour la naissance et suppression

--- src/Bisouland/BeingsBundle/Controller/StatisticsController.php
+++ src/Bisouland/BeingsBundle/Controller/StatisticsController.php
@@ -5,44 +5,10 @@ namespace Bisouland\BeingsBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-use Bisouland\BeingsBundle\Filters\Before;
 use Bisouland\BeingsBundle\Entity\Factory\BeingFactory;
 
-/**
- * @Before("beforeFilter")
- */
 class StatisticsController extends Controller
 {
-    static public $maximumNumberOfBirthInOneDay = 42;
-
-    public function beforeFilter()
-    {
-        $this->generateBirth();
-        $this->removeLosers();
-    }
-    
-    private function generateBirth()
-    {
-        $numberOfBirthsToday = $this->getDoctrine()
-                ->getRepository('BisoulandBeingsBundle:Being')
-                ->countBirthsToday();
-        
-        if (self::$maximumNumberOfBirthInOneDay > $numberOfBirthsToday) {
-            $beingFactory = new BeingFactory($this->get('pronounceable_word_generator'));
-
-            $entityManager = $this->getDoctrine()->getEntityManager();
-            $entityManager->persist($beingFactory->make());
-            $entityManager->flush();
-        }
-    }
-    
-    private function removeLosers()
-    {
-        $this->getDoctrine()
-                ->getRepository('BisoulandBeingsBundle:Being')
-                ->removeLosers();
-    }
-
     /**
      * @Template()
      */

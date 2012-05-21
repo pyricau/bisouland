c9fb1fa999ec210c3742528c2f415a334ece9d49
gnugat <loic.chardonnet@gmail.com>
2012-05-21 12:20:44 +0100

    [Select] Selection dans les vues

--- src/Bisouland/BeingsBundle/Controller/SelectionController.php
+++ src/Bisouland/BeingsBundle/Controller/SelectionController.php
@@ -3,6 +3,7 @@
 namespace Bisouland\BeingsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 use Bisouland\BeingsBundle\Entity\Factory\BeingFactory;
@@ -84,5 +85,23 @@ class SelectionController extends Controller
         $this->getRequest()
                 ->getSession()
                 ->set(self::$sessionKeyForNnameOfBeingSelected, $being->getName());
+    }
+    
+    /**
+     * @Route("/selection/{name}", name="beings_select")
+     */
+    public function selectionAction($name)
+    {
+        $selectedBeing = $this->getDoctrine()
+            ->getRepository('BisoulandBeingsBundle:Being')
+            ->findOneByName($name);
+        
+        if (null !== $selectedBeing) {
+            $this->getRequest()
+                    ->getSession()
+                    ->set(self::$sessionKeyForNnameOfBeingSelected, $name);
+        }
+
+        return $this->redirect($this->generateUrl('homepage'));
     }
 }

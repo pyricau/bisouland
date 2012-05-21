1e7a9be662b93074f3ba373d1b61b242b3235e89
gnugat <loic.chardonnet@gmail.com>
2012-05-21 16:07:36 +0100

    [Attack] Utilisation d'un rapport de combat

--- src/Bisouland/BeingsBundle/RandomSystem/Attack.php
+++ src/Bisouland/BeingsBundle/RandomSystem/Attack.php
@@ -8,33 +8,42 @@ class Attack {
     private $attacker;
     private $defender;
     
+    private $report;
+
     static public $minimumDiceValue = 1;
-    
+
     static public $hitDiceNumberOfFace = 20;
-    
-    
+
     public function __construct(Character $attacker, Character $defender)
     {
         $this->attacker = $attacker;
         $this->defender = $defender;
+        
+        $this->report = array(
+            'attackerRoll' => 0,
+            'defenderRoll' => 0,
+            'hasAttackerHit' => false,
+            'defenderDamages' => 0,
+            'attackerReward' => 0,
+        );
     }
     
     public function hit()
     {
-        $attackerRoll = mt_rand(self::$minimumDiceValue, self::$hitDiceNumberOfFace);
-        $defenderRoll = mt_rand(self::$minimumDiceValue, self::$hitDiceNumberOfFace);
+        $this->report['attackerRoll'] = mt_rand(self::$minimumDiceValue, self::$hitDiceNumberOfFace);
+        $attackerBonus = Character::calculateBonusPointsFromAttributePoints($this->attacker->attack);
+        $attackerScore = $this->report['attackerRoll'] + $attackerBonus;
         
-        $attackerScore = $attackerRoll + Character::calculateBonusPointsFromAttributePoints($this->attacker->attack);
-        $defenderScore = $defenderRoll + Character::calculateBonusPointsFromAttributePoints($this->defender->defense);
+        $this->report['defenderRoll'] = mt_rand(self::$minimumDiceValue, self::$hitDiceNumberOfFace);
+        $defenderBonus = Character::calculateBonusPointsFromAttributePoints($this->defender->defense);
+        $defenderScore = $this->report['defenderRoll'] + $defenderBonus;
         
-        $hasHit = $attackerScore > $defenderScore;
-        if (self::$hitDiceNumberOfFace === $attackerRoll) {
-            $hasHit = true;
+        $this->report['hasAttackerHit'] = $attackerScore > $defenderScore;
+        if (self::$hitDiceNumberOfFace === $this->report['attackerRoll']) {
+            $this->report['hasAttackerHit'] = true;
         }
-        if (self::$minimumDiceValue === $attackerRoll) {
-            $hasHit = false;
+        if (self::$minimumDiceValue === $this->report['attackerRoll']) {
+            $this->report['hasAttackerHit'] = false;
         }
-        
-        return $hasHit;
     }
 }

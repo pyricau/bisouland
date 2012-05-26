71caa16545dd8b949449ed417785274be1efb001
Loic Chardonnet <loic.chardonnet@gmail.com>
2012-05-26 18:56:56 +0100

    [2.2.0] Ajout de la table bonus

--- src/Bisouland/BonusBundle/Controller/DefaultController.php
+++ src/Bisouland/BonusBundle/Controller/DefaultController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+use Bisouland\BeingsBundle\Controller\SelectionController;
+
 class DefaultController extends Controller
 {
     /**
@@ -14,6 +16,20 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
+        $lovePointsToGet = 42 * 42 * 60 * 60;
+        $answer = 'not-enough';
+
+        $selectedBeingName = $this->getRequest()
+                ->getSession()
+                ->get(SelectionController::$sessionKey);
+        $selectedBeing = $this->getDoctrine()
+                ->getRepository('BisoulandBeingsBundle:Being')
+                ->findOneByName($selectedBeingName);
+        
+        if ($selectedBeing->getLovePoints() >= $lovePointsToGet) {
+            $answer = 'success';
+            
+        }
         return array();
     }
 }

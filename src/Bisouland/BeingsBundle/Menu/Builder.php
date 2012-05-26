d820af3f50839d81f988ed584fb62812c1f9a5ad
Loic Chardonnet <loic.chardonnet@gmail.com>
2012-05-26 12:10:35 +0100

    [2.2.0] Ajout des PA dans le menu de selection

--- src/Bisouland/BeingsBundle/Menu/Builder.php
+++ src/Bisouland/BeingsBundle/Menu/Builder.php
@@ -9,21 +9,61 @@ use Bisouland\BeingsBundle\Controller\SelectionController;
 
 class Builder extends ContainerAware
 {
+    private $request;
+    private $menu;
+
     public function selectedMenu(FactoryInterface $factory, array $options)
     {
-        $request = $this->container->get('request');
-        $selectedBeingName = $request->getSession()->get(SelectionController::$sessionKey);
+        $this->request = $request = $this->container->get('request');
+        $selectedBeing = $this->getSelectedBeing();
+        $beingName = $selectedBeing->getName();
 
-        $menu = $factory->createItem('root');
-        $menu->setChildrenAttribute('class', 'nav');
-        $menu->setCurrentUri($request->getRequestUri());
+        $this->menu = $factory->createItem('root');
+        $this->menu->setChildrenAttribute('class', 'nav');
+        $this->menu->setCurrentUri($this->request->getRequestUri());
+        
+        $this->addSelectedNameChild($beingName);
+        $this->addSelectedLovePointsChild($beingName, $selectedBeing->getLovePoints());
 
-        $menu->addChild('selectedBeing', array(
-                    'route' => 'beings_view',
-                    'routeParameters' => array('name' => $selectedBeingName),
-                    'label' => $selectedBeingName,
+        return $this->menu;
+    }
+    
+    private function getSelectedBeing()
+    {
+        $selectedBeingName = $this->request->getSession()->get(SelectionController::$sessionKey);
+        
+        $doctrine = $this->container->get('doctrine');
+        $being = $doctrine->getRepository('BisoulandBeingsBundle:Being')
+                ->findOneByName($selectedBeingName);
+        
+        return $being;
+    }
+
+    private function addSelectedNameChild($name)
+    {
+        $this->menu->addChild('selectedName', array(
+            'route' => 'beings_view',
+            'routeParameters' => array('name' => $name),
+            'label' => $name,
+            'extras' => array('safe_label' => true)
         ));
+    }
 
-        return $menu;
+    private function addSelectedLovePointsChild($name, $lovePoints)
+    {
+        $this->menu->addChild('selectedLovePoints', array(
+            'route' => 'beings_view',
+            'routeParameters' => array('name' => $name),
+            'label' => sprintf(
+                    '%s'
+                    .' <span class="badge">'
+                        .'<abbr class"initialism" title"Points d\'Amour">'
+                            .'PA'
+                        .'</abbr>'
+                    .'</span>',
+                    $lovePoints
+            ),
+            'extras' => array('safe_label' => true)
+        ));
     }
 }

b91c7fe0d3b288cbf769406afc7fa21467fae326
Loic Chardonnet <loic.chardonnet@gmail.com>
2012-05-28 10:46:10 +0100

    [RpgBundle] Activation du bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -27,6 +27,7 @@ class AppKernel extends Kernel
             new Bisouland\FrontBundle\BisoulandFrontBundle(),
             new Bisouland\BeingsBundle\BisoulandBeingsBundle(),
             new Bisouland\BonusBundle\BisoulandBonusBundle(),
+            new Bisouland\RolePlayingGameSystemBundle\BisoulandRolePlayingGameSystemBundle(),
             
             /* Extra Doctrine */
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),

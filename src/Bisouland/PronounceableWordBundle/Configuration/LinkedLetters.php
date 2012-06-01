bf5f312c6c18825c4a3b60c79f9df407f2fff4d4
Loic Chardonnet <loic.chardonnet@gmail.com>
2012-06-01 12:24:24 +0100

    [PronounceableWordBundle] Creation du bundle

---
+++ src/Bisouland/PronounceableWordBundle/Configuration/LinkedLetters.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Bisouland\PronounceableWordBundle\Configuration;
+
+class LinkedLetters {
+    public $lettersWithLinkedLetters = array(
+        'a' => 'bcdefgklmnoprstuvwxz',
+        'b' => 'aehiloru',
+        'c' => 'aehiloru',
+        'd' => 'aehioru',
+        'e' => 'bcdfgiklmnoprstuvwxz',
+        'f' => 'aeiloru',
+        'g' => 'aeiloru',
+        'h' => 'aeiou',
+        'i' => 'abcdefgklmnoprstuvwxz',
+        'k' => 'aeiloru',
+        'l' => 'aeiou',
+        'm' => 'aeiou',
+        'n' => 'aeiou',
+        'o' => 'abcdefgiklmnprstuvwxz',
+        'p' => 'aeiloru',
+        'r' => 'aeiou',
+        's' => 'aehiotu',
+        't' => 'aeiorsu',
+        'u' => 'abcdefgiklmnoprstvwxz',
+        'v' => 'aeiloru',
+        'w' => 'aeiou',
+        'x' => 'aeiou',
+        'z' => 'aeioru',
+    );
+}

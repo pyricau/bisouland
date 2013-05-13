392ab383a99dddd36a19b3c6aaa813e79447a876
Romain <mail@romain.sh>
2013-05-13 22:03:56 +0200

    Fixed .htaccess

--- web/app_dev.php
+++ web/app_dev.php
@@ -21,54 +21,6 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 $request = Request::createFromGlobals();
 
-// Application switcher: if the route does not exist, launch legacy application.
-$urlMatcherPath = __DIR__.'/../app/cache/dev/appDevUrlMatcher.php';
-if (file_exists($urlMatcherPath)) {
-    include $urlMatcherPath;
-
-    $pathInfo = $request->getPathInfo();
-
-    $context = new RequestContext();
-    $context->fromRequest($request);
-    $urlMatcher = new appDevUrlMatcher($context);
-    try {
-        $urlMatcher->match($pathInfo);
-    } catch (ResourceNotFoundException $e) {
-        // The requested route does not exist in the Symfony2 application.
-
-        // Converting routes.
-        $routes = array(
-            '/\/(.+)\.(confirmation)\.html/' => array(1 => 'id', 2 => 'page'),
-            '/\/(.+)\.(envoi)\.html/' => array(1 => 'destinataire', 2 => 'page'),
-            '/\/(.+)\.(bisous)\.html/' => array(1 => 'cancel', 2 => 'page'),
-            '/\/(.+)\.(.+)\.(nuage)\.html/' => array(1 => 'sautnuage', 2 => 'sautposition', 3 => 'page'),
-            '/\/(.+)\.(nuage)\.html/' => array(1 => 'nuage', 2 => 'page'),
-            '/\/(.+)\.(.+)\.(action)\.html/' => array(1 => 'nuage', 2 => 'position', 3 => 'page'),
-            '/\/(.+)\.(.+)\.(yeux)\.html/' => array(1 => 'Dnuage', 2 => 'Dpos', 3 => 'page'),
-            '/\/(.+)\.(.+)\.(newpass)\.html/' => array(1 => 'Cid', 2 => 'Ccle', 3 => 'page'),
-            '/\/(.+)\.(lire)\.html/' => array(1 => 'idmsg', 2 => 'page'),
-            '/\/(livreor)\.(.+)\.html/' => array(1 => 'page', 2 => 'or'),
-            '/\/(membres)\.(.+)\.html/' => array(1 => 'page', 2 => 'num'),
-            '/\/(.+)\.html/' => array(1 => 'page'),
-        );
-        foreach ($routes as $expression => $parameters) {
-            if (preg_match($expression, $pathInfo, $result)) {
-                if (in_array('sautnuage', $parameters)) {
-                    $_GET['saut'] = 1;
-                }
-                foreach ($parameters as $index => $parameter) {
-                    $_GET[$parameter] = $result[$index];
-                }
-                break;
-            }
-        }
-        # RewriteRule (.+)\.confirmation\.html$ /app_dev.php?page=confirmation&id=$1
-
-        include __DIR__.'/index.php'; // Launch the legacy application.
-        exit;
-    }
-}
-
 require_once __DIR__.'/../app/AppKernel.php';
 
 $kernel = new AppKernel('dev', true);

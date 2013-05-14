d5fefa29fda042b084536ffec07ff2a51e08d354
Loic Chardonnet <loic.chardonnet@gmail.com>
2013-05-14 08:35:15 +0200

    [Symfony2] Optimized front controller
    The front controller now generates the Symfony2's cached URL
    matcher if it does not exist, and will initialize the request
    with the cached URL matcher's generated attribute, so the Symfony2
    application won't call the URL matcher again.
    
    Also added some comments to explain the different parts.

--- web/app_dev.php
+++ web/app_dev.php
@@ -4,8 +4,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\RequestContext;
 
-// This check prevents access to debug front controllers that are deployed by accident to production servers.
-// Feel free to remove this, extend it, or make something more sophisticated.
+// Preventing the debug front controller to be available on production servers.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
@@ -15,59 +14,66 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
 }
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
-$request = Request::createFromGlobals();
-
-// Application switcher: if the route does not exist, launch legacy application.
-$urlMatcherPath = __DIR__.'/../app/cache/dev/appDevUrlMatcher.php';
-if (file_exists($urlMatcherPath)) {
-    include $urlMatcherPath;
-
-    $pathInfo = $request->getPathInfo();
-
-    $context = new RequestContext();
-    $context->fromRequest($request);
-    $urlMatcher = new appDevUrlMatcher($context);
-    try {
-        $urlMatcher->match($pathInfo);
-    } catch (ResourceNotFoundException $e) {
-        // The requested route does not exist in the Symfony2 application.
-
-        // Converting routes.
-        $routes = array(
-            '/\/(.+)\.(confirmation)\.html/' => array(1 => 'id', 2 => 'page'),
-            '/\/(.+)\.(envoi)\.html/' => array(1 => 'destinataire', 2 => 'page'),
-            '/\/(.+)\.(bisous)\.html/' => array(1 => 'cancel', 2 => 'page'),
-            '/\/(.+)\.(.+)\.(nuage)\.html/' => array(1 => 'sautnuage', 2 => 'sautposition', 3 => 'page'),
-            '/\/(.+)\.(nuage)\.html/' => array(1 => 'nuage', 2 => 'page'),
-            '/\/(.+)\.(.+)\.(action)\.html/' => array(1 => 'nuage', 2 => 'position', 3 => 'page'),
-            '/\/(.+)\.(.+)\.(yeux)\.html/' => array(1 => 'Dnuage', 2 => 'Dpos', 3 => 'page'),
-            '/\/(.+)\.(.+)\.(newpass)\.html/' => array(1 => 'Cid', 2 => 'Ccle', 3 => 'page'),
-            '/\/(.+)\.(lire)\.html/' => array(1 => 'idmsg', 2 => 'page'),
-            '/\/(livreor)\.(.+)\.html/' => array(1 => 'page', 2 => 'or'),
-            '/\/(membres)\.(.+)\.html/' => array(1 => 'page', 2 => 'num'),
-            '/\/(.+)\.html/' => array(1 => 'page'),
-        );
-        foreach ($routes as $expression => $parameters) {
-            if (preg_match($expression, $pathInfo, $result)) {
-                if (in_array('sautnuage', $parameters)) {
-                    $_GET['saut'] = 1;
-                }
-                foreach ($parameters as $index => $parameter) {
-                    $_GET[$parameter] = $result[$index];
-                }
-                break;
-            }
-        }
-        # RewriteRule (.+)\.confirmation\.html$ /app_dev.php?page=confirmation&id=$1
-
-        include __DIR__.'/index.php'; // Launch the legacy application.
-        exit;
-    }
-}
-
 require_once __DIR__.'/../app/AppKernel.php';
 
 $kernel = new AppKernel('dev', true);
+$request = Request::createFromGlobals();
+
+// Application switcher: if the route does not exist, launch legacy application.
+
+// The cached URL matcher contains the routes of the Symfony2 application.
+$urlMatcherPath = __DIR__.'/../app/cache/dev/appDevUrlMatcher.php';
+if (!file_exists($urlMatcherPath)) { // If the URL matcher isn't already cached...
+    $kernel->boot();
+    $kernel->getContainer()->get('router')->getMatcher(); // ... Then generate it.
+}
+include $urlMatcherPath;
+
+$pathInfo = $request->getPathInfo();
+
+$context = new RequestContext();
+$context->fromRequest($request);
+$urlMatcher = new appDevUrlMatcher($context);
+try {
+    $requestAttributes = $urlMatcher->match($pathInfo);
+} catch (ResourceNotFoundException $e) { // If the route does not exist in the Symfony2 application...
+    // Converting routes.
+    $routes = array(
+        '/\/(.+)\.(confirmation)\.html/' => array(1 => 'id', 2 => 'page'),
+        '/\/(.+)\.(envoi)\.html/' => array(1 => 'destinataire', 2 => 'page'),
+        '/\/(.+)\.(bisous)\.html/' => array(1 => 'cancel', 2 => 'page'),
+        '/\/(.+)\.(.+)\.(nuage)\.html/' => array(1 => 'sautnuage', 2 => 'sautposition', 3 => 'page'),
+        '/\/(.+)\.(nuage)\.html/' => array(1 => 'nuage', 2 => 'page'),
+        '/\/(.+)\.(.+)\.(action)\.html/' => array(1 => 'nuage', 2 => 'position', 3 => 'page'),
+        '/\/(.+)\.(.+)\.(yeux)\.html/' => array(1 => 'Dnuage', 2 => 'Dpos', 3 => 'page'),
+        '/\/(.+)\.(.+)\.(newpass)\.html/' => array(1 => 'Cid', 2 => 'Ccle', 3 => 'page'),
+        '/\/(.+)\.(lire)\.html/' => array(1 => 'idmsg', 2 => 'page'),
+        '/\/(livreor)\.(.+)\.html/' => array(1 => 'page', 2 => 'or'),
+        '/\/(membres)\.(.+)\.html/' => array(1 => 'page', 2 => 'num'),
+        '/\/(.+)\.html/' => array(1 => 'page'),
+    );
+    foreach ($routes as $expression => $parameters) {
+        if (preg_match($expression, $pathInfo, $result)) {
+            if (in_array('sautnuage', $parameters)) {
+                $_GET['saut'] = 1;
+            }
+            foreach ($parameters as $index => $parameter) {
+                $_GET[$parameter] = $result[$index];
+            }
+            break;
+        }
+    }
+    include __DIR__.'/index.php'; // ... Then launch the legacy application.
+    exit;
+}
+
+// Using the generated request attributes by the URL matcher.
+// This will prevent the framework from calling the URL matcher again.
+foreach ($requestAttributes as $key => $value) { 
+    $request->attributes->set($key, $value);
+}
+
+// If the route does exist in the Symfony2 application, then launch it.
 $kernel->loadClassCache();
 Request::enableHttpMethodParameterOverride();
 $response = $kernel->handle($request);

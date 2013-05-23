ab227c254377667ecc4e5695061156d7a0bcafa3
Loïc Chardonnet <loic.chardonnet@gmail.com>
2013-05-23 08:40:50 +0200

    [PreRegistration] Fixed Registration tests

--- tests/acceptance/user/CanRegisterCept.php
+++ tests/acceptance/user/CanRegisterCept.php
@@ -6,10 +6,10 @@ $I->am('Visitor');
 $I->wantTo('register');
 $I->lookForwardTo('see a successful message');
 
-$I->amOnPage('/register');
+$I->amOnPage('/register/');
 $I->fillField('fos_user_registration_form[username]', 'to.register');
 $I->fillField('fos_user_registration_form[email]', 'to.register@example.com');
 $I->fillField('fos_user_registration_form[plainPassword][first]', 'password');
 $I->fillField('fos_user_registration_form[plainPassword][second]', 'password');
-$I->click("S'inscrire");
-$I->see('Vous êtes bien inscrit.');
+$I->click("Enregistrer");
+$I->see('votre compte est maintenant activé.');

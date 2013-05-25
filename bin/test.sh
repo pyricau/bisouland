c347a5eb2d3d079f4f79da9a5790f1c758fbee50
Loïc Chardonnet <loic.chardonnet@gmail.com>
2013-05-25 17:33:29 +0200

    [PreRegistration] Used database initialization in test script

--- bin/test.sh
+++ bin/test.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
+app/console doctrine:database:drop --force --env=test
+app/console doctrine:database:create --env=test
+app/console doctrine:schema:create --env=test
+
+app/console doctrine:fixtures:load --no-interaction --env=test
+
+app/console cache:clear --env=test
+
 bin/codecept build -c app/config/codeception.yml
 bin/codecept run -c app/config/codeception.yml

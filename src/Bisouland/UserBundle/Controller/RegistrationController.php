73a5bd7e2d0f327c7b1d66a1c5cf11e713ba7f4f
Loïc Chardonnet <loic.chardonnet@gmail.com>
2013-06-12 00:35:34 +0200

    [User] Removed registration confirmation

---
+++ src/Bisouland/UserBundle/Controller/RegistrationController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Bisouland\UserBundle\Controller;
+
+use FOS\UserBundle\Controller\RegistrationController as BaseController;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+/**
+ * @author Loic Chardonnet <loic.chardonnet@gmail.com>
+ */
+class RegistrationController extends BaseController
+{
+    public function registerAction()
+    {
+        $form = $this->container->get('fos_user.registration.form');
+        $formHandler = $this->container->get('fos_user.registration.form.handler');
+        $confirmationEnabled = $this->container->getParameter('fos_user.registration.confirmation.enabled');
+
+        $process = $formHandler->process($confirmationEnabled);
+        if ($process) {
+            $user = $form->getData();
+
+            $authUser = false;
+            if ($confirmationEnabled) {
+                $this->container->get('session')->set('fos_user_send_confirmation_email/email', $user->getEmail());
+                $route = 'fos_user_registration_check_email';
+            } else {
+                $authUser = true;
+                $route = 'home';
+            }
+
+            $this->setFlash('fos_user_success', 'registration.flash.user_created');
+            $url = $this->container->get('router')->generate($route);
+            $response = new RedirectResponse($url);
+
+            if ($authUser) {
+                $this->authenticateUser($user, $response);
+            }
+
+            return $response;
+        }
+
+        return $this->container->get('templating')->renderResponse('FOSUserBundle:Registration:register.html.'.$this->getEngine(), array(
+            'form' => $form->createView(),
+        ));
+    }
+}

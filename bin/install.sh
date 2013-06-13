11c64619710ec42de7d89c968bf8274401f81e08
Loïc Chardonnet <loic.chardonnet@gmail.com>
2013-06-13 08:41:32 +0200

    [Doc] added conditional statement to the installation script

--- bin/install.sh
+++ bin/install.sh
@@ -1,25 +1,69 @@
 #!/bin/sh
 
+VHOST=0
+
+usage()
+{
+    cat <<EOT
+Usage:
+    install.sh [--vhost]
+    install.sh -h | --help
+
+Options:
+    --vhost   Create a vhost and log files, restart Apache and add a host
+    -h --help Show this screen
+EOT
+}
+
+if [ $# -gt 1 ]; then
+    echo 'ERROR: too many arguments'
+    usage
+    exit 1
+fi
+
+case $1 in
+    -h | --help)
+        usage
+        exit
+        ;;
+    --vhost)
+        VHOST=1
+        ;;
+    '')
+        ;;
+    *)
+        echo "ERROR: unknown argument \"$1\""
+        usage
+        exit 1
+        ;;
+esac
+
 echo 'Getting the sources'
 git clone git://github.com/pyricau/bisouland.git
 cd bisouland
 
 # Installing requierements
 
-echo 'Getting Composer, the PHP dependency manager'
-curl -sS https://getcomposer.org/installer | php
+if ! type composer; then
+    echo 'Getting Composer, the PHP dependency manager'
+    curl -sS https://getcomposer.org/installer | php
+    sudo mv composer.phar /usr/local/bin/composer
+fi
 
-echo 'Installing bower, the package manager for the web'
-sudo npm install -g bower
+if ! type uglifyjs; then
+    echo 'Installing UglifyJs, the Javascript parser/compressor/beautifier toolkit'
+    sudo npm install -g uglify-js
+fi
 
-echo 'Installing UglifyJs, the Javascript parser/compressor/beautifier toolkit'
-sudo npm install -g uglify-js@1
+if ! type uglifycss; then
+    echo 'Installing UglifyCss, the CSS parser/compressor/beautifier toolkit'
+    sudo npm install -g uglifycss
+fi
 
-echo 'Installing UglifyCss, the CSS parser/compressor/beautifier toolkit'
-sudo npm install -g uglifycss
-
-echo 'Installing LESS, Dynamic stylesheet language (extends CSS)'
-sudo npm install -g less
+if ! type less; then
+    echo 'Installing LESS, Dynamic stylesheet language (extends CSS)'
+    sudo npm install -g less
+fi
 
 # Configuring the project
 
@@ -28,12 +72,12 @@ setfacl -R -m u:www-data:rwx -m u:`whoami`:rwx app/cache app/logs app/sessions
 setfacl -dR -m u:www-data:rwx -m u:`whoami`:rwx app/cache app/logs app/sessions
 
 echo 'Installing the dependencies'
-./composer.phar install --dev
+composer install --dev
 
 # Configuring Apache
-
-echo 'Creating the vhost configuration'
-sudo cat<<EOT | sudo tee /etc/apache2/sites-available/bisouland.local
+if [ $VHOST -eq 1 ]; then
+    echo 'Creating the vhost configuration'
+    sudo cat<<EOT | sudo tee /etc/apache2/sites-available/bisouland.local
 <VirtualHost *:80>
     ServerName bisouland.local
 
@@ -55,16 +99,17 @@ sudo cat<<EOT | sudo tee /etc/apache2/sites-available/bisouland.local
     </Directory>
 </VirtualHost>
 EOT
-sudo sed -i "s|<path>|${PWD}|" /etc/apache2/sites-available/bisouland.local
-sudo ln -s /etc/apache2/sites-available/bisouland.local /etc/apache2/sites-enabled/bisouland.local
+    sudo sed -i "s|<path>|${PWD}|" /etc/apache2/sites-available/bisouland.local
+    sudo ln -s /etc/apache2/sites-available/bisouland.local /etc/apache2/sites-enabled/bisouland.local
 
-echo 'Creating the log files'
-sudo mkdir /var/log/apache2/bisouland
-sudo touch /var/log/apache2/bisouland/error.log
-sudo touch /var/log/apache2/bisouland/access.log
+    echo 'Creating the log files'
+    sudo mkdir /var/log/apache2/bisouland
+    sudo touch /var/log/apache2/bisouland/error.log
+    sudo touch /var/log/apache2/bisouland/access.log
 
-echo 'Restarting the web server'
-sudo /etc/init.d/apache2 restart
+    echo 'Restarting the web server'
+    sudo /etc/init.d/apache2 restart
 
-echo 'Adding the hostname'
-sudo echo '127.0.0.1 bisouland.local' | sudo tee -a /etc/hosts
+    echo 'Adding the hostname'
+    sudo echo '127.0.0.1 bisouland.local' | sudo tee -a /etc/hosts
+fi

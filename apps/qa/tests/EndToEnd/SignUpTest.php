ae78a0ffccaa23412bcd19d952caa87166667a5a
Loïc Faugeron <faugeron.loic@gmail.com>
2025-08-12 16:56:08 +0100

    [v4][qa] covered all Sign Up test cases. I think

--- apps/qa/tests/EndToEnd/SignUpTest.php
+++ apps/qa/tests/EndToEnd/SignUpTest.php
@@ -7,6 +7,7 @@ namespace Bl\Qa\Tests\EndToEnd;
 use Bl\Qa\Tests\Infrastructure\TestKernelSingleton;
 use PHPUnit\Framework\Attributes\CoversNothing;
 use PHPUnit\Framework\Attributes\Large;
+use PHPUnit\Framework\Attributes\TestDox;
 use PHPUnit\Framework\TestCase;
 
 #[CoversNothing]
@@ -18,7 +19,7 @@ final class SignUpTest extends TestCase
         $httpClient = TestKernelSingleton::get()->httpClient();
         $pdo = TestKernelSingleton::get()->pdo();
 
-        $username = 'test_sign_up';
+        $username = 'test_sign_up00';
         $password = 'password';
 
         $httpClient->request('POST', '/inscription.html', [
@@ -31,16 +32,280 @@ final class SignUpTest extends TestCase
         ]);
 
         // Checking and Cleanup
-        $stmt = $pdo->prepare('DELETE FROM membres WHERE pseudo = :pseudo');
+        $stmt = $pdo->prepare('DELETE FROM membres WHERE pseudo = :username');
         $stmt->execute([
-            'pseudo' => $username,
+            'username' => $username,
         ]);
         $deletedRows = $stmt->rowCount();
 
         $this->assertSame(
             1,
             $deletedRows,
-            "Expected 1 newly signed up player to be deleted, but {$deletedRows} were deleted. Sign up has failed.",
+            "Failed asserting that sign up succeeded. Expected 1 newly signed up player to be deleted, but got {$deletedRows}.",
+        );
+    }
+
+    #[TestDox('It prevents usernames that are too short (< 4 characters)')]
+    public function test_it_prevents_usernames_that_are_too_short(): void
+    {
+        $httpClient = TestKernelSingleton::get()->httpClient();
+        $pdo = TestKernelSingleton::get()->pdo();
+
+        $username = 'usr';
+        $password = 'password';
+
+        $httpClient->request('POST', '/inscription.html', [
+            'body' => [
+                'Ipseudo' => $username,
+                'Imdp' => $password,
+                'Imdp2' => $password,
+                'inscription' => "S'inscrire",
+            ],
+        ]);
+
+        // Checking and Cleanup
+        $stmt = $pdo->prepare('DELETE FROM membres WHERE pseudo = :username');
+        $stmt->execute([
+            'username' => $username,
+        ]);
+        $deletedRows = $stmt->rowCount();
+
+        $this->assertSame(
+            0,
+            $deletedRows,
+            "Failed asserting that Username validation succeeded. Expected 0 newly signed up player to be deleted, but got {$deletedRows}.",
+        );
+    }
+
+    #[TestDox('It prevents usernames that are too long (> 15 characters)')]
+    public function test_it_prevents_usernames_that_are_too_long(): void
+    {
+        $httpClient = TestKernelSingleton::get()->httpClient();
+        $pdo = TestKernelSingleton::get()->pdo();
+
+        $username = 'test_sign_up02__';
+        $password = 'password';
+
+        $httpClient->request('POST', '/inscription.html', [
+            'body' => [
+                'Ipseudo' => $username,
+                'Imdp' => $password,
+                'Imdp2' => $password,
+                'inscription' => "S'inscrire",
+            ],
+        ]);
+
+        // Checking and Cleanup
+        $stmt = $pdo->prepare('DELETE FROM membres WHERE pseudo = :username');
+        $stmt->execute([
+            'username' => $username,
+        ]);
+        $deletedRows = $stmt->rowCount();
+
+        $this->assertSame(
+            0,
+            $deletedRows,
+            "Failed asserting that Username validation succeeded. Expected 0 newly signed up player to be deleted, but got {$deletedRows}.",
+        );
+    }
+
+    #[TestDox('It prevents usernames that have special characters (allowed: alpha numerical characters, and undersocre (`_`))')]
+    public function test_it_prevents_usernames_that_have_special_characters(): void
+    {
+        $httpClient = TestKernelSingleton::get()->httpClient();
+        $pdo = TestKernelSingleton::get()->pdo();
+
+        $username = 'test_sign_up03!';
+        $password = 'password';
+
+        $httpClient->request('POST', '/inscription.html', [
+            'body' => [
+                'Ipseudo' => $username,
+                'Imdp' => $password,
+                'Imdp2' => $password,
+                'inscription' => "S'inscrire",
+            ],
+        ]);
+
+        // Checking and Cleanup
+        $stmt = $pdo->prepare('DELETE FROM membres WHERE pseudo = :username');
+        $stmt->execute([
+            'username' => $username,
+        ]);
+        $deletedRows = $stmt->rowCount();
+
+        $this->assertSame(
+            0,
+            $deletedRows,
+            "Failed asserting that Username validation succeeded. Expected 0 newly signed up player to be deleted, but got {$deletedRows}.",
+        );
+    }
+
+    #[TestDox('It prevents usernames that are already used')]
+    public function test_it_prevents_usernames_that_are_already_used(): void
+    {
+        $httpClient = TestKernelSingleton::get()->httpClient();
+        $pdo = TestKernelSingleton::get()->pdo();
+
+        $username = 'test_sign_up04';
+        $password = 'password';
+
+        $httpClient->request('POST', '/inscription.html', [
+            'body' => [
+                'Ipseudo' => $username,
+                'Imdp' => $password,
+                'Imdp2' => $password,
+                'inscription' => "S'inscrire",
+            ],
+        ]);
+        $httpClient->request('POST', '/inscription.html', [
+            'body' => [
+                'Ipseudo' => $username,
+                'Imdp' => $password,
+                'Imdp2' => $password,
+                'inscription' => "S'inscrire",
+            ],
+        ]);
+
+        // Checking and Cleanup
+        $stmt = $pdo->prepare('DELETE FROM membres WHERE pseudo = :username');
+        $stmt->execute([
+            'username' => $username,
+        ]);
+        $deletedRows = $stmt->rowCount();
+
+        $this->assertSame(
+            1,
+            $deletedRows,
+            "Failed asserting that Username validation succeeded. Expected 1 newly signed up player to be deleted, but got {$deletedRows}.",
+        );
+    }
+
+    #[TestDox('It prevents passwords that are too short (< 5 characters)')]
+    public function test_it_prevents_passwords_that_are_too_short(): void
+    {
+        $httpClient = TestKernelSingleton::get()->httpClient();
+        $pdo = TestKernelSingleton::get()->pdo();
+
+        $username = 'test_sign_up05';
+        $password = 'pass';
+
+        $httpClient->request('POST', '/inscription.html', [
+            'body' => [
+                'Ipseudo' => $username,
+                'Imdp' => $password,
+                'Imdp2' => $password,
+                'inscription' => "S'inscrire",
+            ],
+        ]);
+
+        // Checking and Cleanup
+        $stmt = $pdo->prepare('DELETE FROM membres WHERE pseudo = :username');
+        $stmt->execute([
+            'username' => $username,
+        ]);
+        $deletedRows = $stmt->rowCount();
+
+        $this->assertSame(
+            0,
+            $deletedRows,
+            "Failed asserting that Password validation succeeded. Expected 0 newly signed up player to be deleted, but got {$deletedRows}.",
+        );
+    }
+
+    #[TestDox('It prevents passwords that are too long (> 15 characters)')]
+    public function test_it_prevents_passwords_that_are_too_long(): void
+    {
+        $httpClient = TestKernelSingleton::get()->httpClient();
+        $pdo = TestKernelSingleton::get()->pdo();
+
+        $username = 'test_sign_up06';
+        $password = 'passwordthatistoolong';
+
+        $httpClient->request('POST', '/inscription.html', [
+            'body' => [
+                'Ipseudo' => $username,
+                'Imdp' => $password,
+                'Imdp2' => $password,
+                'inscription' => "S'inscrire",
+            ],
+        ]);
+
+        // Checking and Cleanup
+        $stmt = $pdo->prepare('DELETE FROM membres WHERE pseudo = :username');
+        $stmt->execute([
+            'username' => $username,
+        ]);
+        $deletedRows = $stmt->rowCount();
+
+        $this->assertSame(
+            0,
+            $deletedRows,
+            "Failed asserting that Password validation succeeded. Expected 0 newly signed up player to be deleted, but got {$deletedRows}.",
+        );
+    }
+
+    #[TestDox('It prevents passwords that have special characters (allowed: alpha numerical characters, and undersocre (`_`))')]
+    public function test_it_prevents_passwords_that_have_special_characters(): void
+    {
+        $httpClient = TestKernelSingleton::get()->httpClient();
+        $pdo = TestKernelSingleton::get()->pdo();
+
+        $username = 'test_sign_up07';
+        $password = 'password!';
+
+        $httpClient->request('POST', '/inscription.html', [
+            'body' => [
+                'Ipseudo' => $username,
+                'Imdp' => $password,
+                'Imdp2' => $password,
+                'inscription' => "S'inscrire",
+            ],
+        ]);
+
+        // Checking and Cleanup
+        $stmt = $pdo->prepare('DELETE FROM membres WHERE pseudo = :username');
+        $stmt->execute([
+            'username' => $username,
+        ]);
+        $deletedRows = $stmt->rowCount();
+
+        $this->assertSame(
+            0,
+            $deletedRows,
+            "Failed asserting that Password validation succeeded. Expected 0 newly signed up player to be deleted, but got {$deletedRows}.",
+        );
+    }
+
+    public function test_it_prevents_passwords_that_do_not_match_confirmation(): void
+    {
+        $httpClient = TestKernelSingleton::get()->httpClient();
+        $pdo = TestKernelSingleton::get()->pdo();
+
+        $username = 'test_sign_up08';
+        $password = 'password';
+        $passwordConfirmation = 'different';
+
+        $httpClient->request('POST', '/inscription.html', [
+            'body' => [
+                'Ipseudo' => $username,
+                'Imdp' => $password,
+                'Imdp2' => $passwordConfirmation,
+                'inscription' => "S'inscrire",
+            ],
+        ]);
+
+        // Checking and Cleanup
+        $stmt = $pdo->prepare('DELETE FROM membres WHERE pseudo = :username');
+        $stmt->execute([
+            'username' => $username,
+        ]);
+        $deletedRows = $stmt->rowCount();
+
+        $this->assertSame(
+            0,
+            $deletedRows,
+            "Failed asserting that Password validation succeeded. Expected 0 newly signed up player to be deleted, but got {$deletedRows}.",
         );
     }
 }

fe023fa008a1f93688728bb1fac4effe631ca8e0
Loïc Faugeron <faugeron.loic@gmail.com>
2025-08-14 07:07:04 +0100

    [v4][qa] added test account cleanup at the end of the testsuite

---
+++ apps/qa/tests/Infrastructure/Scenario/DeleteAllTestPlayers.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bl\Qa\Tests\Infrastructure\Scenario;
+
+use Bl\Qa\Tests\Infrastructure\TestKernelSingleton;
+
+final readonly class DeleteAllTestPlayers
+{
+    public static function run(): void
+    {
+        $pdo = TestKernelSingleton::get()->pdo();
+
+        $pdo->query("DELETE FROM membres WHERE pseudo LIKE 'BisouTest%'");
+    }
+}

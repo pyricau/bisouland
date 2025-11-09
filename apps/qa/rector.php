37c50a708079ff373e2ad695ba39709eef49f8ff
Loïc Faugeron <faugeron.loic@gmail.com>
2025-11-09 17:31:28 +0000

    [qa] Added rector to apply automated refactorings when necessary

---
+++ apps/qa/rector.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\SetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/tests',
+        __DIR__ . '/../monolith/web',
+    ])
+    ->withSkip([
+        __DIR__ . '/../monolith/web/ban',
+        __DIR__ . '/../monolith/web/images',
+        __DIR__ . '/../monolith/web/includes',
+        __DIR__ . '/../monolith/web/polices',
+        __DIR__ . '/../monolith/web/smileys',
+    ])
+    ->withSets([
+        SetList::PHP_56,
+    ]);

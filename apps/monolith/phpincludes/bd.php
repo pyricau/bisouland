3be2a3fa9522d93510b7f87a82c1afc1a4e16593
Loïc Faugeron <faugeron.loic@gmail.com>
2025-11-21 19:46:45 +0000

    [v4] Configured PDO with persistent connection enabled
    Without persistent connections,
    we're creating and tearing down a new database connection for every request.
    
    That's expensive overhead.
    
    This proves to be especially detrimental for PostgreSQL,
    which we'll migrate to, so let's fix that now.

--- apps/monolith/phpincludes/bd.php
+++ apps/monolith/phpincludes/bd.php
@@ -12,6 +12,7 @@ function bd_connect()
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES => false,
+            PDO::ATTR_PERSISTENT => true,
         ];
         $pdo = new PDO($dsn, DATABASE_USER, DATABASE_PASSWORD, $options);
     }
--- apps/qa/tests/Infrastructure/TestKernel.php
+++ apps/qa/tests/Infrastructure/TestKernel.php
@@ -31,6 +31,12 @@ final readonly class TestKernel
             "mysql:host={$dbHost};port={$dbPort};dbname={$dbName}",
             $dbUser,
             $dbPass,
+            [
+                \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+                \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+                \PDO::ATTR_EMULATE_PREPARES => false,
+                \PDO::ATTR_PERSISTENT => true,
+            ],
         );
 
         return new self(

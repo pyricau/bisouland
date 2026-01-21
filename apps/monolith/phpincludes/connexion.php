a2dc65861938552968024b6ff01a57e0aacace81
Loïc Faugeron <faugeron.loic@gmail.com>
2026-01-21 18:23:36 +0000

    [v4] Made BisouLand stateless by removing Session usage

--- apps/monolith/phpincludes/connexion.php
+++ apps/monolith/phpincludes/connexion.php
@@ -1,22 +1,24 @@
-<h1>Connexion</h1>
 <?php
-if (false === $blContext['is_signed_in']) {
-    if (isset($_SESSION['errCon'])) {
-        echo '<br />',$_SESSION['errCon'],'<br /><br />';
-        unset($_SESSION['errCon']);
-    }
-
-    ?>
-
-<form method="post" class="formul" action="connexion.html">
-	<label>Pseudo :<br /><span class="petit">(Entre 4 et 15 caractères)</span><br /><input type="text" name="pseudo" tabindex="10" size="15" maxlength="15" value=""/></label><br />
-	<label>Mot de passe : <br /><span class="petit">(Entre 5 et 15 caractères)</span><br /><input type="password" name="mdp" tabindex="20" size="15" maxlength="15" value=""/></label><br />
-    <label><input type="checkbox" checked="checked" name="auto" />Connexion automatique</label><br />
-	<input type="submit" name="connexion" value="Se connecter" /><br /><br />
-</form>
-
-<?php
-} else {
-    echo "T'es dja connected !!";
-}
+$errorMessage = match ($_GET['e'] ?? '') {
+    '1' => 'Erreur : le mot de passe est incorrect !',
+    '2' => "Erreur : le pseudo n'existe pas !",
+    '3' => 'Erreur : vous avez oublié de remplir un ou plusieurs champs !',
+    '4' => 'Erreur : vous devez être connecté pour vous déconnecter !',
+    default => null,
+};
 ?>
+
+<h1>Connexion</h1>
+<?php if (false === $blContext['is_signed_in']) { ?>
+    <?php if (null !== $errorMessage) { ?>
+        <br /><?php echo $errorMessage; ?><br /><br />
+    <?php } ?>
+    <form method="post" class="formul" action="connexion.html">
+        <label>Pseudo :<br /><span class="petit">(Entre 4 et 15 caractères)</span><br /><input type="text" name="pseudo" tabindex="10" size="15" maxlength="15" value=""/></label><br />
+        <label>Mot de passe : <br /><span class="petit">(Entre 5 et 15 caractères)</span><br /><input type="password" name="mdp" tabindex="20" size="15" maxlength="15" value=""/></label><br />
+        <label><input type="checkbox" checked="checked" name="auto" />Connexion automatique</label><br />
+        <input type="submit" name="connexion" value="Se connecter" /><br /><br />
+    </form>
+<?php } else { ?>
+    T'es dja connected !!
+<?php } ?>

68161855d5acb6da7b95661fce92886a74b7245f
Loïc Faugeron <faugeron.loic@gmail.com>
2026-02-18 08:18:23 +0000

    [qalin] use Upgradable enum cases for select options on Web page

--- apps/qa/src/UserInterface/Web/Action/InstantFreeUpgradeController.php
+++ apps/qa/src/UserInterface/Web/Action/InstantFreeUpgradeController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Bl\Qa\UserInterface\Web\Action;
 
+use Bl\Qa\Domain\Game\Player\UpgradableLevels\Upgradable;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Twig\Environment;
@@ -18,6 +19,8 @@ final readonly class InstantFreeUpgradeController
     #[Route('/actions/instant-free-upgrade', methods: ['GET'])]
     public function __invoke(): Response
     {
-        return new Response($this->twig->render('actions/instant-free-upgrade.html.twig'));
+        return new Response($this->twig->render('actions/instant-free-upgrade.html.twig', [
+            'upgradables' => Upgradable::cases(),
+        ]));
     }
 }
